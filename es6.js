c0de85f76ac6a34fce69b5bb9226b5bd14d017bf
kapicaluk <kapicaluk@gmail.com>
2017-08-15 12:14:15 -0400

    added var

--- es6.js
+++ es6.js
@@ -1,3 +1,10 @@
 var animal = {
-	
+	species: 'dog',
+	weigth: 23,
+	sound: woof, 
 }
+
+var { species, sound } = animal
+console.log('The ' + species + ' says ' + sounds + '!');
+
+var anothoerVar = [];
